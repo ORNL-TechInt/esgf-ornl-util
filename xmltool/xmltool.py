bd80c20404795e9e6c83359efe996537ed29fe1c
Tom Barron <tbarron@ornl.gov>
2013-01-11 17:50:07 -0500

    Catch exceptions in attrlist, improve geneaology()

--- xmltool/xmltool.py
+++ xmltool/xmltool.py
@@ -4,6 +4,7 @@ import pdb
 import re
 import sys
 import toolframe
+import traceback as tb
 import xml.etree.ElementTree as ET
 
 from optparse import *
@@ -69,35 +70,41 @@ def xml_attrlist(argv):
 
     Matching attributes are listed.
     """
-    p = OptionParser()
-    p.add_option('-d', '--debug',
-                 action='store_true', default=False, dest='debug',
-                 help='run the debugger')
-    p.add_option('-a', '--attr',
-                 action='store', default=None, dest='attrib',
-                 help='attribute to edit')
-    (o, a) = p.parse_args(argv)
+    try:
+        p = OptionParser()
+        p.add_option('-d', '--debug',
+                     action='store_true', default=False, dest='debug',
+                     help='run the debugger')
+        p.add_option('-a', '--attr',
+                     action='store', default=None, dest='attrib',
+                     help='attribute to edit')
+        (o, a) = p.parse_args(argv)
 
-    if o.debug: pdb.set_trace()
+        if o.debug: pdb.set_trace()
 
-    for filename in a:
-        X = ET.parse(filename)
-        elements = []
-        for el in X.iter():
-            elements.append(el)
-            
-        for el in X.iter():
-            if o.attrib in el.attrib.keys():
-                path = geneaology(el, elements, X)
-                print("%s -> %s: %s" % (path, o.attrib, el.attrib[o.attrib]))
+        for filename in a:
+            X = ET.parse(filename)
+            elements = []
+            for el in X.iter():
+                elements.append(el)
 
+            tagl = [e.tag for e in elements]
+            pfx = os.path.commonprefix(tagl)
+            for el in X.iter():
+                if o.attrib in el.attrib.keys():
+                    path = geneaology(el, elements, X, pfx)
+                    print("%s -> %s: %s" % (path, o.attrib, el.attrib[o.attrib]))
+    except Exception, e:
+        tb.print_exc(e)
 
 # ---------------------------------------------------------------------------
-def geneaology(el, elements, root):
+def geneaology(el, elements, root, pfx):
     rval = ''
     p = find_parent(el, elements)
-    while p not in root._children:
-        rval = '%s > ' % (p.tag) + rval
+    # while p not in root._children:
+    while p != None and p not in root._root._children:
+        tag = p.tag.replace(pfx, '')
+        rval = '%s > ' % (tag) + rval
         p = find_parent(p, elements)
     return rval
 
