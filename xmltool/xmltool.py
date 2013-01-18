573229fd660978ddd4094afc8ecaae70a31f6c65
Tom Barron <tbarron@ornl.gov>
2013-01-18 17:49:10 -0500

    Building ability to add a stanza inside an XML file to enable adding SRM support to a pre-existing THREDDS file.

--- xmltool/xmltool.py
+++ xmltool/xmltool.py
@@ -10,6 +10,49 @@ import xml.etree.ElementTree as ET
 from optparse import *
 
 # ---------------------------------------------------------------------------
+def xml_add(argv):
+    """add - add information to an xml file
+
+    usage: xmltool add [-d] -f input-info-file <filename> <filename> ...
+
+    -d     run under python debugger
+    -f     filename containing info to be added
+
+    Result is written to stdout.
+    """
+    p = OptionParser()
+    p.add_option('-d', '--debug',
+                 action='store_true', default=False, dest='debug',
+                 help='run the debugger')
+    p.add_option('-f', '--input-file',
+                 action='store', default=None, dest='infile',
+                 help='name of file containing info to be added')
+    (o, a) = p.parse_args(argv)
+
+    if o.debug: pdb.set_trace()
+
+    add = ET.parse(o.infile)
+
+    for filename in a:
+        # X = parse_map1(filename)
+        X = ET.parse(filename)
+        set_global_namespace(X)
+                              
+        for el in X.iter():
+            if o.attrib in el.attrib.keys():
+                # -- do the edit
+                ebuf = el.attrib[o.attrib]
+                for eval in ehash.keys():
+                    ebuf = ebuf.replace(eval, ehash[eval])
+
+                el.attrib[o.attrib] = ebuf
+
+        p = ET.ProcessingInstruction('xml', 'version="1.0" encoding="UTF-8"')
+        sys.stdout.write(ET.tostring(p) + "\n")
+        X.write(sys.stdout)
+        print("")
+    
+# ---------------------------------------------------------------------------
 def xml_attredit(argv):
     """attredit - edit attribute values in XML files
 
@@ -100,6 +143,37 @@ def xml_attrlist(argv):
     except Exception, e:
         tb.print_exc(e)
 
+# ---------------------------------------------------------------------------
+def xml_iterate(argv):
+    """iterate - iterate an xml file and report what we get
+
+    usage: xmltool iterate [-d] <filename> <filename> ...
+
+    -d     run under python debugger
+
+    This routine is for iterating the contents of an xml file
+    """
+    p = OptionParser()
+    p.add_option('-d', '--debug',
+                 action='store_true', default=False, dest='debug',
+                 help='run the debugger')
+    (o, a) = p.parse_args(argv)
+
+    if o.debug: pdb.set_trace()
+
+
+    for filename in a:
+        tree = ET.parse(filename)
+        elements = [el for el in tree.iter()]
+        pfx = os.path.commonprefix([e.tag for e in tree.iter()])
+        for el in tree.iter():
+            tagstr = geneaology(el, elements, tree, pfx)
+            print tagstr
+            for atr in el.attrib.keys():
+                print '   %s -> %s' % (atr, el.attrib[atr])
+            if el.text != None and el.text.strip() != '':
+                print '   TEXT: %s' % (el.text)
+            
 # ---------------------------------------------------------------------------
 def xml_tparse(argv):
     """tparse - test parsing
